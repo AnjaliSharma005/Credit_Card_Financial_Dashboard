1536c2fc16445671d505a89dbf2adb2619fd3e61
AnjaliSharma005 <005anjsharma@gmail.com>
2025-03-06 07:22:57 -0800

    Add files via upload

---
+++ Credit_Card_SQL_Queries.sql
@@ -0,0 +1,100 @@
+--1.cc_detail_table---
+CREATE TABLE cc_detail (
+    Client_Num INT,
+    Card_Category VARCHAR(20),
+    Annual_Fees INT,
+    Activation_30_Days INT,
+    Customer_Acq_Cost INT,
+    Week_Start_Date DATE,
+    Week_Num VARCHAR(20),
+    Qtr VARCHAR(10),
+    current_year INT,
+    Credit_Limit DECIMAL(10,2),
+    Total_Revolving_Bal INT,
+    Total_Trans_Amt INT,
+    Total_Trans_Ct INT,
+    Avg_Utilization_Ratio DECIMAL(10,3),
+    Use_Chip VARCHAR(10),
+    Exp_Type VARCHAR(50),
+    Interest_Earned DECIMAL(10,3),
+    Delinquent_Acc VARCHAR(5)
+);
+
+SET datestyle = 'DMY';
+
+
+--2. Create customer_detail_table---
+CREATE TABLE cust_detail (
+    Client_Num INT,
+    Customer_Age INT,
+    Gender VARCHAR(5),
+    Dependent_Count INT,
+    Education_Level VARCHAR(50),
+    Marital_Status VARCHAR(20),
+    State_cd VARCHAR(50),
+    Zipcode VARCHAR(20),
+    Car_Owner VARCHAR(5),
+    House_Owner VARCHAR(5),
+    Personal_Loan VARCHAR(5),
+    Contact VARCHAR(50),
+    Customer_Job VARCHAR(50),
+    Income INT,
+    Cust_Satisfaction_Score INT
+);
+--3.Import data cutomer_detail---
+COPY cust_detail FROM 'D:\csv\chameli\cust_add.csv'DELIMITER ',' CSV HEADER;
+
+--4.Import data cc_detail---
+COPY cc_detail FROM 'D:\csv\chameli\cc_add.csv'DELIMITER ',' CSV HEADER;
+
+select * from cust_detail;
+select * from cc_detail;
+
+--cc_add---
+CREATE TABLE cc_add (
+    Client_Num INT,
+    Card_Category VARCHAR(20),
+    Annual_Fees INT,
+    Activation_30_Days INT,
+    Customer_Acq_Cost INT,
+    Week_Start_Date DATE,
+    Week_Num VARCHAR(20),
+    Qtr VARCHAR(10),
+    current_year INT,
+    Credit_Limit DECIMAL(10,2),
+    Total_Revolving_Bal INT,
+    Total_Trans_Amt INT,
+    Total_Trans_Ct INT,
+    Avg_Utilization_Ratio DECIMAL(10,3),
+    Use_Chip VARCHAR(10),
+    Exp_Type VARCHAR(50),
+    Interest_Earned DECIMAL(10,3),
+    Delinquent_Acc VARCHAR(5)
+);
+
+SET datestyle = 'DMY';
+
+COPY cc_add FROM 'D:\csv\chameli\cc_add.csv'DELIMITER ',' CSV HEADER;
+select * from cc_add;
+
+--cust_add----
+CREATE TABLE cust_add (
+    Client_Num INT,
+    Customer_Age INT,
+    Gender VARCHAR(5),
+    Dependent_Count INT,
+    Education_Level VARCHAR(50),
+    Marital_Status VARCHAR(20),
+    State_cd VARCHAR(50),
+    Zipcode VARCHAR(20),
+    Car_Owner VARCHAR(5),
+    House_Owner VARCHAR(5),
+    Personal_Loan VARCHAR(5),
+    Contact VARCHAR(50),
+    Customer_Job VARCHAR(50),
+    Income INT,
+    Cust_Satisfaction_Score INT
+);
+
+COPY cust_add FROM 'D:\csv\chameli\cust_add.csv'DELIMITER ',' CSV HEADER;
+select * from cc_add;
